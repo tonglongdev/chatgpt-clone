466e50c789f2d491d2c29a59b4ac78665121ba3b
TongLongDev <tonglong.dev@gmail.com>
2025-09-30 18:50:48 +0700

    Homepage css

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,60 @@
 import { useUser } from "@auth0/nextjs-auth0/client";
 import Head from "next/head";
 import Link from "next/link";
+// import { getSession } from "@auth0/nextjs-auth0";
+import { faRobot } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Home() {
   const { isLoading, error, user } = useUser();
 
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>{error.message}</div>;
-  
+
   return (
-    <div>
+    <>
       <Head>
-        <title>Next JS ChatGPT Starter</title>
+        <title>ChatGPT - Login or Signup</title>
       </Head>
-      <h1>Welcome to the Next JS &amp; ChatGPT Starter</h1>
-      <div className="mt-4 flex justify-center gap-3">
-        {!user && (
-          <>
-            <Link href="/api/auth/login" className="btn">
-              Login
-            </Link>
-            <Link href="/api/auth/signup" className="btn">
-              Signup
-            </Link>
-          </>
-        )}
+      <div className="flex min-h-screen w-full items-center justify-center bg-gray-800 text-center text-white">
+        <div>
+          <div>
+            <FontAwesomeIcon
+              icon={faRobot}
+              className="mb-2 text-6xl text-emerald-200"
+            />
+          </div>
+          <h1 className="text-4xl font-bold">Welcome to ChatGPT</h1>
+          <p className="mt-2 text-lg">Log in with your account to continue</p>
+          <div className="mt-4 flex justify-center gap-3">
+            {!user && (
+              <>
+                <Link href="/api/auth/login" className="btn">
+                  Login
+                </Link>
+                <Link href="/api/auth/signup" className="btn">
+                  Signup
+                </Link>
+              </>
+            )}
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
+
+// export const getServerSideProps = async (ctx) => {
+//   const session = await getSession(ctx.req, ctx.res);
+//   if (!!session) {
+//     return {
+//       redirect: {
+//         destination: "/chat",
+//       },
+//     };
+//   }
+
+//   return {
+//     props: {},
+//   };
+// };
